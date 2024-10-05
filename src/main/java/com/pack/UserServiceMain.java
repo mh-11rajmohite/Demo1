972b4180a117efb8f0b72110e022c85d919961c3
MOHITE RAJ <mohiteraj647@gmail.com>
2024-10-05 16:38:00 +0530

    Initial Commit

---
+++ src/main/java/com/pack/UserServiceMain.java
@@ -0,0 +1,18 @@
+package com.pack;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserServiceMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("user.xml");
+
+		UserService u = ctx.getBean(UserService.class);
+		u.register();
+		
+	}
+
+}
