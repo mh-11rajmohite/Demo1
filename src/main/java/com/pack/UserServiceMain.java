746141c0aa47005fc8d273e732143e200c7ec0c0
MOHITE RAJ <mohiteraj647@gmail.com>
2024-10-05 16:53:36 +0530

    Added sysout in UserMain

--- src/main/java/com/pack/UserServiceMain.java
+++ src/main/java/com/pack/UserServiceMain.java
@@ -8,6 +8,7 @@ public class UserServiceMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	System.out.println("Welcome");
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("user.xml");
 
 		UserService u = ctx.getBean(UserService.class);
