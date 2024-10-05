0d10c2ea40cfa37498598ea2a3ba740019d7c6a0
Raj Mohite <59164432+mh-11rajmohite@users.noreply.github.com>
2024-10-05 16:41:08 +0530

    Update PlayerMain.java

--- src/main/java/com/pack/PlayerMain.java
+++ src/main/java/com/pack/PlayerMain.java
@@ -8,6 +8,7 @@ public class PlayerMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		public static final int x = 10;
 		ApplicationContext c = new FileSystemXmlApplicationContext("player.xml");
 		Player p = (Player) c.getBean("player");
 		System.out.println(p.getName() + " " + p.getId()+ " " + p.getSport().getName());
